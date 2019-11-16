496165626a50725028f2e5daba94bbe9949fda37
Yves Zumbach <yves.zumbach@gmail.com>
2019-11-16 21:02:53 +0100

    Create first solidity contract

---
+++ backend/micropinion.sol
@@ -0,0 +1,82 @@
+pragma solidity >=0.4.22 <0.7.0;
+pragma experimental ABIEncoderV2;
+
+contract Microdemocracy {
+
+    uint8 constant MAX_ALTERNATIVES_PER_POLL = 10;
+    
+    struct Opinion {
+        bool voted;
+        uint8[] ranking;
+    }
+
+    struct Poll {
+        string name;
+        string description;
+        uint256 closingMoment;
+        bool active;
+        address owner;
+        string[] alternatives;
+        uint8 alternativesCount;
+        mapping(address => Opinion) opinions;
+    }
+
+    Poll[] public polls;
+
+    function createPoll(
+        string memory pollName,
+        string memory pollDescription,
+        uint256 closingMoment,
+        string[] memory alternativeNames
+    ) public {
+        require(alternativeNames.length < MAX_ALTERNATIVES_PER_POLL, "Too many alternative provided.");
+
+        polls.push(Poll({
+            name: pollName,
+            description: pollDescription,
+            closingMoment: closingMoment,
+            active: true,
+            owner: msg.sender,
+            alternatives: alternativeNames,
+            alternativesCount: uint8(alternativeNames.length)
+        }));
+    }
+
+    function giveOpinion(uint128 pollUid, uint8[] memory ranking) public {
+        require(pollUid < polls.length, "You cannot vote for a non-existant poll.");
+        
+        Poll storage poll = polls[pollUid];
+
+        require(poll.active, "You cannot vote for an already closed poll.");
+        require(block.timestamp <= poll.closingMoment, "You cannot vote for this poll as it is already closed.");
+        require(poll.opinions[msg.sender].voted, "You cannot vote more than one time.");
+        require(ranking.length <= poll.alternativesCount, "The provided ranking is not valid.");
+
+        for (uint8 i = 0; i < ranking.length; i++) {
+            require(ranking[i] < poll.alternativesCount, "You provided at least one invalid entry in the ranking");
+        }
+
+        for (uint8 i = 0; i < ranking.length; i++) {
+            for (uint8 j = i + 1; j < ranking.length; j++) {
+                require(ranking[i] != ranking[j], "You cannot have twice the same alternative in you ranking.");
+            }
+        }
+
+        poll.opinions[msg.sender] = Opinion({
+            voted: true,
+            ranking: ranking
+        });
+    }
+
+    function closePoll(uint128 pollUid) public {
+        require(pollUid < polls.length, "You cannot close a non-existant poll.");
+        require(polls[pollUid].active, "You cannot close an already-closed poll.");
+        
+        Poll storage poll = polls[pollUid];
+
+        require(msg.sender == poll.owner, "You cannot close a poll that you do not own.");
+        require(block.timestamp > poll.closingMoment, "You cannot close a poll whose closing moment is in the future.");
+        
+        poll.active = false;
+    }
+}
