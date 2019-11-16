77fff4ef436105af8ea78ca695570c97f08a4e23
Robin Mamié <robin@mamie.one>
2019-11-16 22:49:41 +0100

    Fix connection to MetaMask

--- frontend/src/util/getWeb3.js
+++ frontend/src/util/getWeb3.js
@@ -4,30 +4,29 @@ import Web3 from 'web3'
 * 1. Check for injected web3 (mist/metamask)
 * 2. If metamask/mist create a new web3 instance and pass on result
 * 3. Get networkId - Now we can check the user is connected to the right network to use our dApp
-* 4. Get user account from metamask
+* 4. Get user account from metamask + authorization
 * 5. Get user balance
 */
 
 let getWeb3 = new Promise(function (resolve, reject) {
   // Check for injected web3 (mist/metamask)
-  var web3js = window.web3
-  if (typeof web3js !== 'undefined') {
-    var web3 = new Web3(web3js.currentProvider)
-    resolve({
-      injectedWeb3: web3.isConnected(),
-      web3 () {
-        return web3
-      }
-    })
-  } else {
-    // web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545')) GANACHE FALLBACK
-    reject(new Error('Unable to connect to Metamask'))
-  }
+
+  var web3 = new Web3(window.ethereum)
+  web3.eth.net.isListening((err, isListening) => {
+    if (!err) {
+      resolve({
+        injectedWeb3: isListening,
+        web3 () {
+          return web3
+        }
+      })
+    }
+  })
 })
   .then(result => {
     return new Promise(function (resolve, reject) {
       // Retrieve network ID
-      result.web3().version.getNetwork((err, networkId) => {
+      result.web3().eth.net.getId((err, networkId) => {
         if (err) {
           // If we can't find a networkId keep result the same and reject the promise
           reject(new Error('Unable to retrieve network ID'))
@@ -41,6 +40,14 @@ let getWeb3 = new Promise(function (resolve, reject) {
   })
   .then(result => {
     return new Promise(function (resolve, reject) {
+      // Gain access to account
+      window.ethereum.enable((err, accounts) => {
+        if (err) {
+          reject(new Error('Unable to access account'))
+        } else {
+          console.log(accounts)
+        }
+      })
       // Retrieve coinbase
       result.web3().eth.getCoinbase((err, coinbase) => {
         if (err) {
@@ -59,6 +66,7 @@ let getWeb3 = new Promise(function (resolve, reject) {
         if (err) {
           reject(new Error('Unable to retrieve balance for address: ' + result.coinbase))
         } else {
+          balance = parseFloat(balance) * 1e-18
           result = Object.assign({}, result, { balance })
           resolve(result)
         }
