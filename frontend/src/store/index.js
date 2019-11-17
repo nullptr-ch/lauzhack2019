5521d9ca4daf1cbed166a5112c7a47ae922a5409
Yves Zumbach <yves.zumbach@gmail.com>
2019-11-17 04:53:51 +0100

    Factorize store/index.js

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,5 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import state from './state'
-import getWeb3 from '../util/getWeb3'
-
-import abi from 'assets/microdemocracy-abi.json'
-
-const contractAddress = '0xdfdEfF7650a96A6ded29c8c7E6eE060c3eEaa4Ac'
 
 Vue.use(Vuex)
 
@@ -17,37 +11,11 @@ Vue.use(Vuex)
 export default function (/* { ssrContext } */) {
   const Store = new Vuex.Store({
     strict: true,
-    state,
-    mutations: {
-      registerWeb3Instance (state, payload) {
-        let result = payload
-        let web3Copy = state.web3
-        web3Copy.coinbase = result.coinbase
-        web3Copy.networkId = result.networkId
-        web3Copy.balance = parseInt(result.balance, 10)
-        web3Copy.isInjected = result.injectedWeb3
-        web3Copy.web3Instance = result.web3
-        state.web3 = web3Copy
-
-        console.log(abi)
-        const Contract = web3Copy.web3Instance().eth.Contract
-        console.log('a')
-        state.contractInstance = new Contract(abi, contractAddress)
-        console.log('b')
-      }
+    modules: {
+      ethengine,
+      // contract,
     },
-    actions: {
-      registerWeb3 ({ commit }) {
-        console.log('registerWeb3 Action being executed')
-        getWeb3.then(result => {
-          console.log('committing result to registerWeb3Instance mutation')
-          commit('registerWeb3Instance', result)
-        }).catch(e => {
-          console.log('error in action registerWeb3', e)
-        })
-      }
-    }
-  })
+  });
 
-  return Store
+  return Store;
 }
